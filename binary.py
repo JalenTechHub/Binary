0fceb41e1ef712451905fe0d6ed7fc2b4c795523
Jalen Joseph <169100912+JalenTechHub@users.noreply.github.com>
2024-12-27 21:28:41 -0500

    Update binary.py
    Adding input from user to start.

--- binary.py
+++ binary.py
@@ -1,12 +1,12 @@
 while True:
-    binary = "1010101"
+    binary = input("Enter a binary number")
     if all(char in "01" for char in binary):
         break
     print("Invalid input.")
 denary = 0
 
-# Loop through the binary string from right to left using a simple index
+
 for i in range(len(binary)):
-    if binary[len(binary) - 1 - i] == "1":  # Access characters from the end
+    if binary[len(binary) - 1 - i] == "1":  
         denary += 2**i
 print(denary)
